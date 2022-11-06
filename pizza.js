2dbb4b9c85be66efa98489b05bcce4b2574b5781
Shahshan20 <104528639+Shahshan20@users.noreply.github.com>
2022-11-07 02:05:30 +0500

    Add files via upload

---
+++ pizza.js
@@ -0,0 +1,124 @@
+const storeList = [
+    {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [73.096379,33.525429]
+        },
+        "properties": {
+          "name": "outlet 1",
+          "address": "Shop number 67a Waraich Tower Bahria Spring North Phase Rawalpindi, Rawalpindi, 46220, Pakistan",
+          "phone": "+92 21 111 339 339"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [ 67.307891,25.031364]
+        },
+        "properties": {
+          "name": "outlet 2",
+          "address": "B - 33 Midway B, Bahria Town Karachi, Karachi, Karachi City, Sindh 75340, Pakistan",
+          "phone": "+92 21 111 339 339"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [67.08795530253086,24.92806628342919]
+        },
+        "properties": {
+          "name": "outlet 3",
+          "address": "Plot No 567، Main Allama Shabbir Ahmed Usmani Rd، near Samdani Hospital, Block 5 Gulshan-e-Iqbal, Karachi, Karachi City, Sindh, Pakistan",
+          "phone": "+92 21 111 339 339"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [ 74.1522951479395,31.476799553265746]
+        },
+        "properties": {
+          "name": "outlet 4",
+          "address": "Shop # G-4 & F-4 Al-Fazal Mall, Canal Bank Rd, opp. Sector A, Bahria Town, Lahore, Pakistan",
+          "phone": "+92 21 111 339 339"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [ 74.38787341361194,31.67356586477988,]
+        },
+        "properties": {
+          "name": "outlet 5",
+          "address": "Shop # 7-8 Bazar Stop, Shalimar Link Road, near Ramgarh, Lahore, Punjab 54000, Pakistan",
+          "phone": "+92 21 111 339 339"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [ 66.9276059215474,30.56646615613389]
+        },
+        "properties": {
+          "name": "outlet 6",
+          "address": "Quetta, Balochistan 12345, Pakistan",
+          "phone": "+92 21 111 339 339"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [ 67.12902861433817,25.060495748161436]
+        },
+        "properties": {
+          "name": "outlet 7",
+          "address": "Sector x3 Sector X Gulshan-e-Maymar, Karachi, Karachi City, Sindh 75340, Pakistan",
+          "phone": "+92 21 111 339 339"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [ 68.36145897012644,25.42400458446779]
+        },
+        "properties": {
+          "name": "outlet 8",
+          "address": "Shop no 22/A RECHS, Auto Bahn Rd, Hyderabad, Pakistan",
+          "phone": "+92 21 111 339 339"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [ 73.07554745450628,33.67405527697201]
+        },
+        "properties": {
+          "name": "outlet 9",
+          "address": "Shop no.3 on Ground floor Fantom Mall Plot no. 16 Sector, I-8 Markaz I 8 Markaz I-8, Islamabad, Islamabad Capital Territory, Pakistan",
+          "phone": "+92 21 111 339 339"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [ 73.0651223637655,31.42529866464577]
+        },
+        "properties": {
+          "name": "outlet 10",
+          "address": "Building number, P-281 Street No. 1, Gobind Pura Gulberg, Faisalabad, Punjab, Pakistan",
+          "phone": "+92 21 111 339 339"
+        }
+      },
+     
+   
+]
